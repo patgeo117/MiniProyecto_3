ce7328b174e3a88d28675930a293086754c9c3e1
J-Serna2 <jhojan.serna@correounivalle.edu.co>
2023-06-16 22:45:29 -0500

    10:45

--- src/Main/Principal.java
+++ src/Main/Principal.java
@@ -1,22 +1,11 @@
 package Main;
 
-import Bibliotecarios.*;
 import Interfaz.InLogin;
 
-import Bibliotecarios.UsuarioMaestro;
 public class Principal {
-    //public static String rutaArchivo = "src/Archivos_Bin/Libros.bin";
     public static void main(String[] args) {
-        // InCrearUs.setDataM();
-        //ManejoArchivo.getDataRow(rutaArchivo);
-        //ManejoArchivo.CrearArchivo(rutaArchivo);
+
         new InLogin();
 
-       /* UsuarioMaestro usuarioMaestro = new UsuarioMaestro("Master", "Master");
-        Employees[] nuevosEmployees = new Employees[1];
-        nuevosEmployees[0] = usuarioMaestro;
-
-        SerizalizaDeseralizaUs serizalizaDeseralizaUs = new SerizalizaDeseralizaUs();
-        serizalizaDeseralizaUs.setDataB(nuevosEmployees, "src/Archivos_Bin/dataM.bin");*/
     }
 }
